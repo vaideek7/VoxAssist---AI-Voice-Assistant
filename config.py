c5cf8b67df4c971d68f7fcfd1c0fff9c7bb8dee3
vaideek7 <101199221+vaideek7@users.noreply.github.com>
2023-10-11 19:48:19 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+# todo: Add your api key here
+apikey = "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"
---
+++ main.py
@@ -0,0 +1,129 @@
+import speech_recognition as sr
+import os
+import webbrowser
+import openai
+from config import apikey
+import datetime
+import random
+import numpy as np
+import pyttsx3
+
+
+chatStr = ""
+
+
+
+"""def say(text):
+    os.system(f'say "{text}"')
+"""
+
+
+def say(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+
+def chat(query):
+    global chatStr
+    print(chatStr)
+    # "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"
+    openai.api_key = apikey
+    chatStr += f"You: {query}\n vox asisst: "
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=chatStr,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    # todo: Wrap this inside of a  try catch block
+    say(response["choices"][0]["text"])
+    chatStr += f"{response['choices'][0]['text']}\n"
+    return response["choices"][0]["text"]
+
+
+def ai(prompt):
+    openai.api_key = apikey
+    text = f"OpenAI response for Prompt: {prompt} \n *************************\n\n"
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    # todo: Wrap this inside of a  try catch block
+    # print(response["choices"][0]["text"])
+    text += response["choices"][0]["text"]
+    if not os.path.exists("Openai"):
+        os.mkdir("Openai")
+
+    # with open(f"Openai/prompt- {random.randint(1, 2343434356)}", "w") as f:
+    with open(f"Openai/{''.join(prompt.split('intelligence')[1:]).strip() }.txt", "w") as f:
+        f.write(text)
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        # r.pause_threshold =  0.6
+        audio = r.listen(source)
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said: {query}")
+            return query
+        except Exception as e:
+            return "Some Error Occurred. Sorry from vox asisst"
+
+
+if __name__ == '__main__':
+    print('Welcome to Vox Asisst A.I')
+    say("Vox Asisst A.I")
+    while True:
+        print("Listening...")
+        query = takeCommand()
+        # todo: Add more sites
+        sites = [["youtube", "https://www.youtube.com"], ["wikipedia",
+                                                          "https://www.wikipedia.com"], ["google", "https://www.google.com"],]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                say(f"Opening {site[0]} sir...")
+                webbrowser.open(site[1])
+        # todo: Add a feature to play a specific song
+        if "open music" in query:
+            musicPath = "/Users/vdk15/Downloads/downfall-21371.mp3"
+            os.system(f"open {musicPath}")
+
+        elif "the time" in query:
+            musicPath = "/Users/vdk15/Downloads/downfall-21371.mp3"
+            hour = datetime.datetime.now().strftime("%H")
+            min = datetime.datetime.now().strftime("%M")
+            say(f"Sir time is {hour} and {min} minutes")
+
+        elif "open facetime".lower() in query.lower():
+            os.system(f"open /System/Applications/FaceTime.app")
+
+        elif "open pass".lower() in query.lower():
+            os.system(f"open /Applications/Passky.app")
+
+        elif "Using artificial intelligence".lower() in query.lower():
+            ai(prompt=query)
+
+        elif "vox asisst Quit".lower() in query.lower():
+            exit()
+
+        elif "reset chat".lower() in query.lower():
+            chatStr = ""
+
+        else:
+            print("Chatting...")
+            chat(query)
+
+        # say(query)
